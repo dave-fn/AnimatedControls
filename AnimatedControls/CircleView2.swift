ef968110fa27a8bf65c12e6e370be56ed2db3586
David Negrete <davidfnegrete@gmail.com>
2015-06-26 19:39:02 -0600

    Added a pie slice as a CAShapeLayer and implemented CALayerDelegate methods to manipulate the animations
    However, this doesn't yield appropriate results as the framework shifts between the old and new path values and
    the animation morphs between the states, but does not yield the desired result.
    This requires subclassing CALayer (or CAShapeLayer)

--- AnimatedControls/CircleView2.swift
+++ AnimatedControls/CircleView2.swift
@@ -14,6 +14,11 @@ class CircleView2 : NSView {
   var containerLayer : CALayer
   var layer1 : CALayer
   var layer2 : CAShapeLayer
+  var arcLayer : CAShapeLayer
+  
+  var arcEndAngle : CGFloat
+  
+  let π = CGFloat(M_PI)
   
   
   // MARK: - Initializers
@@ -42,6 +47,11 @@ class CircleView2 : NSView {
     layer2 = CAShapeLayer()
     layer2.name = "layer-shape-2"
     
+    arcLayer = CAShapeLayer()
+    arcLayer.name = "layer-shape_arc"
+    
+    arcEndAngle = π / 2.0
+    
     super.init(coder: coder)
     
     doInitialSetup()
@@ -62,14 +72,47 @@ class CircleView2 : NSView {
     
     containerLayer.addSublayer(layer1)
     containerLayer.addSublayer(layer2)
+    containerLayer.addSublayer(arcLayer)
+    
+//    layer1.delegate = self
+//    layer2.delegate = self
+    arcLayer.delegate = self
   }
   
   
   func createCircleLayer() {
+    
+    drawLayer1()
+    drawLayer2()
+    drawArcLayer()
+    
+  }
+  
+  func updateCircles() {
+    layer1.borderWidth += 5.0
+    
+    layer2.fillColor = NSColor.redColor().CGColor
+    layer2.lineWidth += 1.0
+    
+//    arcLayer.lineWidth += 10.0
+    arcEndAngle += π / 4.0
+    arcLayer.setNeedsDisplay()
+  }
+  
+  func adjustAngles() {
+  }
+  
+  
+  // MARK: - Individual Drawing
+  func drawLayer1() {
+    // Layer 1 - CALayer
     layer1.frame = CGRectInset(self.bounds, 10, 10)
     layer1.borderColor = NSColor.yellowColor().CGColor
     layer1.borderWidth = 1.0
-    
+  }
+  
+  func drawLayer2() {
+    // Layer 2 - CAShapeLayer
     layer2.frame = CGRectInset(self.bounds, 10, 10)
     
 //    var center = CGPoint(x: self.bounds.size.width/2, y: self.bounds.size.height/2)
@@ -91,15 +134,122 @@ class CircleView2 : NSView {
     layer2.fillColor = NSColor.whiteColor().CGColor
   }
   
-  func updateCircles() {
-    layer1.borderWidth += 5.0
+  func drawArcLayer() {
+    // Arc - CAShapeLayer
+    arcLayer.frame = CGRectInset(self.bounds, 10, 10)
     
-    layer2.fillColor = NSColor.redColor().CGColor
-    layer2.lineWidth += 1.0
+    var radius : CGFloat = 95.0
+    var startAngle = π / 4.0
+    var endAngle = arcEndAngle // π / 2.0
+    var clockwise : Bool = false
+    
+    var aPath = CGPathCreateMutable()
+    var center = CGPoint(x: NSMidX(self.bounds), y: NSMidY(self.bounds))
+    CGPathMoveToPoint(aPath, nil, center.x, center.y)
+    CGPathAddArc(aPath, nil, center.x, center.y, radius, startAngle, endAngle, clockwise)
+    
+    arcLayer.path = aPath
+    
+    arcLayer.fillColor = NSColor.blackColor().CGColor
+    
+    arcLayer.lineWidth = 2.0
+    arcLayer.strokeColor = NSColor.whiteColor().CGColor
   }
   
-  func adjustAngles() {
+  
+  // MARK: - Individual Animations
+  func animateLayer1(key: String!) -> CABasicAnimation {
+//    println("animating layer 1")
+    
+    var animation = CABasicAnimation(keyPath: key)
+    
+    if layer1.presentationLayer()?.valueForKey(key) != nil {
+      animation.fromValue = layer1.presentationLayer().valueForKey(key)
+    }
+    else {
+      animation.fromValue = 0
+    }
+    
+    animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    animation.duration = 2.25
+    
+    return animation
   }
+  
+  func animateLayer2(key: String!) -> CABasicAnimation {
+//    println("animating layer 2")
+    
+    var animation = CABasicAnimation(keyPath: key)
+    
+    if layer2.presentationLayer()?.valueForKey(key) != nil {
+      animation.fromValue = layer2.presentationLayer().valueForKey(key)
+    }
+    else {
+      animation.fromValue = 0
+    }
+    
+    animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+    animation.duration = 5.25
+    
+    return animation
+  }
+  
+  func animateArcLayer(key: String!) -> CABasicAnimation {
+//    println("animating arc layer")
+    
+    var animation = CABasicAnimation(keyPath: key)
+    
+    if arcLayer.presentationLayer()?.valueForKey(key) != nil {
+      animation.fromValue = arcLayer.presentationLayer().valueForKey(key)
+    }
+    else {
+      animation.fromValue = 0
+    }
+    
+    animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+    animation.duration = 1.0
+    
+    return animation
+  }
+  
+  
+  // MARK: - CALayerDelegate
+//  override func displayLayer(layer: CALayer!) {
+//    println("in delegate - displayLayer")
+////    super.displayLayer(layer)
+//    layer.displayIfNeeded()
+//  }
+  
+  override func drawLayer(layer: CALayer!, inContext ctx: CGContext!) {
+    println("in delegate - drawLayer")
+    
+    if layer == arcLayer {
+      drawArcLayer()
+    }
+    
+    super.drawLayer(layer, inContext: ctx)
+  }
+  
+  override func actionForLayer(layer: CALayer!, forKey key: String!) -> CAAction! {
+    
+    if layer == layer1 {
+      return animateLayer1(key)
+    }
+    else if layer == layer2 {
+      return animateLayer2(key)
+    }
+    else if layer == arcLayer {
+      println("the key for the arc is \(key)")
+      return animateArcLayer(key)
+    }
+    
+    return super.actionForLayer(layer, forKey: key)
+  }
+  
+//  override func layoutSublayersOfLayer(layer: CALayer!) {
+//    println("in delegate - layoutSublayersOfLayer")
+//    super.layoutSublayersOfLayer(layer)
+//  }
   
   
 }
